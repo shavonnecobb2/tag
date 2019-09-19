b0224a7ed21a6052579ace1e231aed57d91b9184
Shavonne Cobb <Shavonne.Cobb@improving.com>
2019-09-19 10:20:54 -0500

    removing constructors from Game

--- src/main/java/org/improving/tag/Game.java
+++ src/main/java/org/improving/tag/Game.java
@@ -9,16 +9,6 @@ public class Game {
     private Date endTime;
 
 
-    public Game(int maxInventory) {
-        System.out.println("Ran the constructor, Maximum Inventory is " + maxInventory);
-    }
-
-    public Game(int maxInventory, int playerAge) {
-        this(maxInventory);
-        System.out.println("The player's age is " + playerAge);
-    }
-
-
 
     public Date getStartTime() { return startTime; }
     private void setStartTime (Date startTime) {
